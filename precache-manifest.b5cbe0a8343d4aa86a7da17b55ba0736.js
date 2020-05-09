6678016a0aa63277ea6d9e670a29037ed0f4eb1b
Viktoria-Gunia <58438642+Viktoria-Gunia@users.noreply.github.com>
2020-05-09 19:40:32 +0300

    Add files via upload

---
+++ precache-manifest.b5cbe0a8343d4aa86a7da17b55ba0736.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6063f3d8772cc3aff237ad81f262a0c",
+    "url": "/Viktoria-Gunia/react-app.git/index.html"
+  },
+  {
+    "revision": "63ccf1256ebcc0f0094d",
+    "url": "/Viktoria-Gunia/react-app.git/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "f3b0059d2c0ecefdf6e5",
+    "url": "/Viktoria-Gunia/react-app.git/static/css/main.72f372f7.chunk.css"
+  },
+  {
+    "revision": "63ccf1256ebcc0f0094d",
+    "url": "/Viktoria-Gunia/react-app.git/static/js/2.8c943274.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Viktoria-Gunia/react-app.git/static/js/2.8c943274.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3b0059d2c0ecefdf6e5",
+    "url": "/Viktoria-Gunia/react-app.git/static/js/main.d3e611c6.chunk.js"
+  },
+  {
+    "revision": "7d9250fe28dd361a8f99",
+    "url": "/Viktoria-Gunia/react-app.git/static/js/runtime-main.ba3a8339.js"
+  }
+]);
